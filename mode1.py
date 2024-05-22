56cbb6941a1681fbad89e2a7fc8eecb7b80427b4
rjusni <161101621+rjusni@users.noreply.github.com>
2024-05-23 08:52:58 +1000

    Pass all tests cases

--- mode1.py
+++ mode1.py
@@ -1,4 +1,7 @@
 from landsites import Land
+from data_structures.heap import MaxHeap
+from algorithms.mergesort import mergesort
+from data_structures.referential_array import ArrayR
 
 
 class Mode1Navigator:
@@ -6,27 +9,146 @@ class Mode1Navigator:
     Student-TODO: short paragraph as per
     https://edstem.org/au/courses/14293/lessons/46720/slides/318306
     """
+   
 
     def __init__(self, sites: list[Land], adventurers: int) -> None:
         """
         Student-TODO: Best/Worst Case
+        Complexity: best/worst O(n log n)
         """
-        raise NotImplementedError()
+        self.sites = sites 
+        self.adventurers = adventurers
+        
+        #site_order holds lands in list ordered from most valuable to least
+        self.site_order = self.heapsort_land()
+
+    def heapsort_land(self):
+        """Sorts the lands from best ratio to worst using heapsort
+    
+        Complexity:
+        First loop iterates through all sites and creates a tuple with the reward ratio and land, which is then appended to array - O(n)
+        The array is then converted to a maxheap using heapify - O(n)
+        The lands are then spat back out in order using heap get_max() (log n) and then appended. This loop runs n times for each land - O(n log n)
+
+        Final Complexity:
+        O(n) + O(n) + O(n log n)
+        = O(n log n) where n is the number of lands
+
+        Completeness of a heap means 
+        """
+
+        value_array = []
+
+        #O(n)
+        for site in self.sites:
+            #Finds the value of gold to adventure sent for each island and adds to a max heap
+            value = (site.get_gold()/site.get_guardians(), site)
+            value_array.append(value)        
+
+        #Heapify O(n)
+        site_heap = MaxHeap.heapify(value_array)
+
+        res = []
+
+        while len(site_heap) != 0:
+            val, land_val = site_heap.get_max()
+            res.append(land_val)
+
+        return res 
+
 
     def select_sites(self) -> list[tuple[Land, int]]:
         """
         Student-TODO: Best/Worst Case
+
+        Complexity:
+        Loop runs maximum n times where n is the number off lands. If after the first iteration,  there are no more adventurers, the loop enters the final if statment and breaks.
+            Best Case: First island uses all troops for best value
+                O(1)
+            Worst Case: Need to go through all islands which had value calculated during init
+                O(n)
         """
-        raise NotImplementedError()
+        res = []
+
+        adventurers_left = self.adventurers
+
+        #O(n)
+        for item in self.site_order:
+            guardians_cost = item.get_guardians()
+
+            if guardians_cost < adventurers_left:
+                res.append((item, guardians_cost))
+            else:
+                res.append((item, adventurers_left))
+
+            adventurers_left -= guardians_cost
+            if adventurers_left < 0:
+                break
+
+        return res
+
 
     def select_sites_from_adventure_numbers(self, adventure_numbers: list[int]) -> list[float]:
         """
-        Student-TODO: Best/Worst Case
+        Complexity:
+            Best and Worst Case of last two loops add up to be O(A) for both cases.
+            Mergesort of A log A dominates O(A)
+            Complexity also scales with O(n)
+        Final Complexity:
+            O(A log A) + O(N) where A is length of adventure_numbers and N is number of lands
         """
-        raise NotImplementedError()
+        res = [None] * len(adventure_numbers)
+
+        #Used to remember original order to reformat result
+        arr_remember = []
+        #O(A)
+        for i in range(len(adventure_numbers)):
+            val = (adventure_numbers[i], i)
+            arr_remember.append(val)
+        
+        #O(A log A)
+        sorted_arr_remb = mergesort(arr_remember)
+        
+        curr_index = 0
+        curr_largest = sorted_arr_remb[curr_index][0]
+        curr_total = 0
+
+        adventurers_used = 0
+
+        #O(n)
+        for item in self.site_order:
+
+            guardians_cost = item.get_guardians()
+
+            #Use of counter means while loop runs independent to the for loop and will run maximum A times
+            #Best Case: curr largest is already bigger than cost so while loop is never entered O(1)
+            #Worst case: needs to run for each adventure_number O(A)
+            while curr_largest - adventurers_used <= guardians_cost:
+                #While current adventuers cannot fully ransack an island
+                res[sorted_arr_remb[curr_index][1]] = ((curr_largest-adventurers_used)/guardians_cost)*item.get_gold() + curr_total
+                curr_index += 1
+                try:
+                    curr_largest = sorted_arr_remb[curr_index][0]
+                except:
+                    #filled all of list
+                    break
+
+            adventurers_used += guardians_cost
+            curr_total += item.get_gold()
+
+
+        #Check if any adventure numbers are over the total of all lands
+        #Best Case All items were filled in previous loop O(1)
+        #Worst Case: all items need to be filled here O(A)
+        while curr_index < len(adventure_numbers):
+            res[sorted_arr_remb[curr_index][1]] = curr_total
+            curr_index += 1
+
+        return res
 
     def update_site(self, land: Land, new_reward: float, new_guardians: int) -> None:
         """
-        Student-TODO: Best/Worst Case
+        Complexity: O(1)
         """
-        raise NotImplementedError()
+        land.set_gold(new_reward)
+        land.set_guardians(new_guardians)
--- mode2.py
+++ mode2.py
@@ -1,4 +1,5 @@
 from landsites import Land
+from data_structures.heap import MaxHeap
 
 
 class Mode2Navigator:
@@ -9,18 +10,93 @@ class Mode2Navigator:
 
     def __init__(self, n_teams: int) -> None:
         """
-        Student-TODO: Best/Worst Case
+        Complexity: O(1)
         """
-        raise NotImplementedError()
+        self.teams_count = n_teams
+        self.sites = []
+
 
     def add_sites(self, sites: list[Land]) -> None:
         """
-        Student-TODO: Best/Worst Case
+        Complexity of adding and copying two lists is the cost of iterating over both of them
+        Complexity:
+        O(N) + O(S) = O(N+S) where N is the size of pre-existing list and S is size of new list
         """
-        raise NotImplementedError()
+        self.sites = self.sites + sites
+
 
     def simulate_day(self, adventurer_size: int) -> list[tuple[Land | None, int]]:
         """
-        Student-TODO: Best/Worst Case
+        Complexity:
+        Setting up the sites and ordering them in the heap is O(N) + O(N) which is just O(N)
+
+        The day logic loop runs maximum K times where K is the number of teams and has important functions heap get_max and add which are O(log N) 
+
+        Final Complexity:
+            O(N) + O(K) * O(log N)
+            = O(N + K log N) where N is the number of lands and K is the number of teams
+
         """
-        raise NotImplementedError()
+
+        #populate a max heap with all the lands
+        #use heapify to maintain complexity requirement
+        score_arr = []
+        #O(N) 
+        for land in self.sites:
+            score = self.calculate_score(adventurer_size, land)
+            score_arr.append(score)
+        
+        #O(N)
+        score_heap = MaxHeap.heapify(score_arr)
+        #The max element will be the most valuable that sends the most troops
+        #most troops is better as it occupies potential lands for enemies
+
+        res = []
+
+        #O(K)
+        for _ in range(self.teams_count):
+            out = (None, 0)
+            #O(log N)
+            optimal = score_heap.get_max()
+            #optimal is a tuple with (Gold, Troops sent, Land)
+            opt_gold, opt_troop, opt_land = optimal
+            out = (opt_land, opt_troop)
+            res.append(out)
+
+            #Replace land with ransacked land
+            if opt_land is not None:
+                opt_land.set_gold(opt_land.get_gold() - optimal[0])
+                opt_land.set_guardians(opt_land.get_guardians() - optimal[1])
+                new_land_score = self.calculate_score(adventurer_size, opt_land)
+                #O(log N)
+                score_heap.add(new_land_score)
+
+        return res
+
+
+    def calculate_score(self, adventurers_tot: int, land: Land):
+        """Calculates the max score for a given land
+        
+            returns (optimal_score, troops required to send)
+
+        Complexity:
+        All operations are artithmetic or constant.
+        Best case if only first if statement needs to be checked which is one arithmetic comparison
+        Final Complexity: O(1)
+        """
+
+        "If a troop doesn't earn more than 2.5 it isn't worth to send."
+        if land.get_guardians() == 0 or (land.get_gold()/land.get_guardians()) < 2.5:
+            #Not worth to send, just stay all at home
+            score = (2.5 * adventurers_tot, 0, None)
+        elif adventurers_tot <= land.get_guardians():
+            #Can send all troops to island
+            score = ((adventurers_tot/land.get_guardians())*land.get_gold(), adventurers_tot, land)
+        else:
+            #Send what we can
+            score = (land.get_gold() + 2.5*(adventurers_tot - land.get_guardians()),land.get_guardians(), land)
+        return score
+
+
+
+
